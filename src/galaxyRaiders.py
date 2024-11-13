84754eef4c8cc203b4e5bfa8902cd5d0bcad3d4e
faekxng <aeharper1221@gmail.com>
2024-11-13 16:16:46 -0600

    player moves

--- src/galaxyRaiders.py
+++ src/galaxyRaiders.py
@@ -1,26 +1,9 @@
 import pygame
 
 
-def player_controller(screen):
+def player_controller(screen, playerX, playerY):
         player_image = pygame.image.load('space-invaders.png')
-        playerX = 370
-        playerY = 480
-        playerX_change = 0
-        playerY_change = 0
         screen.blit(player_image, (playerX, playerY))
-
-#if keystroke is pressed, check if left or right
-def button_pressed(event):
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            playerX_change = -0.3
-        if event.key == pygame.K_RIGHT:
-            playerX_change = 0.3
-        playerX += playerX_change
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-            playerX_change = 0
-        playerX += playerX_change
 
 
 
@@ -30,15 +13,33 @@ def main():
     pygame.display.set_caption("Galaxy Raiders")
     resolution = (800, 600)
     screen = pygame.display.set_mode((resolution))
+    playerX = 370
+    playerY = 480
+    playerX_change = 0
+    playerY_change = 0
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+    #if keystroke is pressed, check if left or right and move player
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= 736:
+            playerX = 736
+    #render
         black = pygame.Color(0,0,0)
         screen.fill (black)
-        player_controller(screen)
-        button_pressed(event)
+        playerX += playerX_change
+        player_controller(screen, playerX, playerY)
         pygame.display.update()
     pygame.quit()
 
