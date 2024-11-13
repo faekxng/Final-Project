dd237334a421cef23d3f72eb2913afaf7d03c469
faekxng <aeharper1221@gmail.com>
2024-11-12 18:12:41 -0600

    cant access local variable where it is not associated with a value

--- src/galaxyRaiders.py
+++ src/galaxyRaiders.py
@@ -5,7 +5,24 @@ def player_controller(screen):
         player_image = pygame.image.load('space-invaders.png')
         playerX = 370
         playerY = 480
+        playerX_change = 0
+        playerY_change = 0
         screen.blit(player_image, (playerX, playerY))
+
+#if keystroke is pressed, check if left or right
+def button_pressed(event):
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            playerX_change = -0.3
+        if event.key == pygame.K_RIGHT:
+            playerX_change = 0.3
+        playerX += playerX_change
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            playerX_change = 0
+        playerX += playerX_change
+
+
 
 def main():
     #Initialize pygame 
@@ -21,8 +38,10 @@ def main():
         black = pygame.Color(0,0,0)
         screen.fill (black)
         player_controller(screen)
+        button_pressed(event)
         pygame.display.update()
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
