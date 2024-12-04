5d743237accba747756514af9b26b0235e5d64df
faekxng <aeharper1221@gmail.com>
2024-12-04 13:02:16 -0600

    bullet still wont fire

--- src/galaxyRaiders.py
+++ src/galaxyRaiders.py
@@ -18,10 +18,10 @@ def fire_bullet(screen, x, y):
     global bullet_state
     bullet_image = pygame.image.load('paintball.png')
     bullet_state = "fire"
-    screen.blit(bullet_image, (x, y, x + 16, y + 10))
+    screen.blit(bullet_image, (x + 16, y + 10))
 
 
-def game_over():
+def game_over(screen):
     game_over_text = game_over_font.render("GAME OVER", True, (255,255,255))
     screen.blit(game_over_text, (190, 250))
 
@@ -29,29 +29,35 @@ def game_over():
 
 
 def main():
+
 #Initialize pygame 
     pygame.init()
     pygame.display.set_caption("Galaxy Raiders")
     resolution = (800, 600)
     screen = pygame.display.set_mode((resolution))
     background = pygame.image.load('spaceBackground.jpg')
+
 #player
     playerX = 370
     playerY = 480
     playerX_change = 0
     playerY_change = 0
     player_hit_box = pygame.Rect(playerX, playerY,  playerX + 64, playerY - 64)
+
 #enemy
     enemyX = random.randint(0,800)
     enemyY = random.randint(50,150)
     enemyX_change = 0.3 
     enemyY_change = 40
     enemy_hit_box = pygame.Rect(enemyX, enemyY, enemyX + 64, enemyY - 64)
+
 #bullet
-    bulletY = 480
+    global bullet_state
+    bulletY = 380
     bulletY_change = 10
     bulletX_change = 0
     bullet_state = "ready"
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -63,13 +69,13 @@ def main():
                     playerX_change = -.3
                 if event.key == pygame.K_RIGHT:
                     playerX_change = .3
-                if event.key == pygame.K_SPACE:
-                    if bullet_state == "ready":
-                        bulletX = playerX
-                        fire_bullet(screen, bulletX, bulletY)
+                if event.key == pygame.K_SPACE and bullet_state == "ready":
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     playerX_change = 0
+
     #boundaries for player and enemies
         if playerX <= 0:
             playerX = 0
@@ -81,6 +87,7 @@ def main():
         elif enemyX >= 736:
             enemyX_change = -0.2
             enemyY += enemyY_change
+
     #bullet movement
         if bulletY <= 0:
             bulletY = 380
@@ -88,6 +95,7 @@ def main():
         if bullet_state == "fire":
             fire_bullet(bulletX, bulletY)
             bulletY -= bulletY_change
+
     #collision and game over
         collide = pygame.Rect.colliderect(player_hit_box, enemy_hit_box)
         """
@@ -96,8 +104,9 @@ def main():
         else:
             return False
         if collide == True:
-            game_over()
+            game_over(screen)
         """
+
     #render
         black = pygame.Color(0,0,0)
         screen.fill (black)
