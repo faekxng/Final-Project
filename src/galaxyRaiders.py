04f6f6164baf4c7bade2b902457838cb6c682a55
faekxng <aeharper1221@gmail.com>
2024-12-04 10:43:28 -0600

    changed playerX to x etc and minor changes

--- src/galaxyRaiders.py
+++ src/galaxyRaiders.py
@@ -1,26 +1,25 @@
 import pygame
 import random
 
-def player_controller(screen, playerX, playerY):
+def player_controller(screen, x, y):
 
     player_image = pygame.image.load('space-invaders.png')
-    screen.blit(player_image, (playerX, playerY))
-    player_hit_box = Rect(playerX + 64, playerY - 64)
+    screen.blit(player_image, (x, y))
+    
 
-
-def enemy(screen, enemyX, enemyY):
+def enemy(screen, x, y):
 
     enemy_image = pygame.image.load('ufo.png')
-    screen.blit(enemy_image, (enemyX, enemyY))
-    enemy_hit_box = Rect(enemyX + 64, enemyY - 64)
+    screen.blit(enemy_image, (x, y))
+    
 
-
-def fire_bullet(screen, playerX, playerY):  
+def fire_bullet(screen, x, y):  
     
     global bullet_state
     bullet_image = pygame.image.load('paintball.png')
     bullet_state = "fire"
-    screen.blit(bullet_image, (playerX + 16, playerY + 10))
+    screen.blit(bullet_image, (x, y, x + 16, y + 10))
+
 
 def game_over():
     game_over_text = game_over_font.render("GAME OVER", True, (255,255,255))
@@ -41,13 +40,13 @@ def main():
     playerY = 480
     playerX_change = 0
     playerY_change = 0
-    player_hit_box = Rect(playerX + 64, playerY - 64)
+    player_hit_box = pygame.Rect(playerX, playerY,  playerX + 64, playerY - 64)
 #enemy
     enemyX = random.randint(0,800)
     enemyY = random.randint(50,150)
     enemyX_change = 0.3 
     enemyY_change = 40
-    enemy_hit_box = Rect(enemyX + 64, enemyY - 64)
+    enemy_hit_box = pygame.Rect(enemyX, enemyY, enemyX + 64, enemyY - 64)
 #bullet
     bulletY = 480
     bulletY_change = 10
@@ -67,7 +66,7 @@ def main():
                 if event.key == pygame.K_SPACE:
                     if bullet_state == "ready":
                         bulletX = playerX
-                        fire_bullet(screen, playerX, playerY)
+                        fire_bullet(screen, bulletX, bulletY)
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     playerX_change = 0
@@ -84,18 +83,21 @@ def main():
             enemyY += enemyY_change
     #bullet movement
         if bulletY <= 0:
-            bulletY = 480
+            bulletY = 380
             bullet_state = "ready"
         if bullet_state == "fire":
             fire_bullet(bulletX, bulletY)
             bulletY -= bulletY_change
     #collision and game over
         collide = pygame.Rect.colliderect(player_hit_box, enemy_hit_box)
+        """
         if collide:
-            gameOver = True
-            return gameOver
-        if gameOver = True:
+            return True
+        else:
+            return False
+        if collide == True:
             game_over()
+        """
     #render
         black = pygame.Color(0,0,0)
         screen.fill (black)
